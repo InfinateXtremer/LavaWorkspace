0ab9a4da65f0982f26f2cae9c32b249d5f71d9e3
Koenji <shenonreduxz@gmail.com>
2021-04-03 02:37:44 +0100

    LavaPlayerStart C++ And LavaWorldSettings
    Adds more c++ Lava components that the game uses. On a test branch as i accidentally built it in VS2017. Hopefully nothing breaks.

---
+++ Source/Lava/LavaPlayerStart.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerStart.h"
+#include "LavaPlayerStart.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LAVA_API ALavaPlayerStart : public APlayerStart
+{
+	GENERATED_BODY()
+public: 
+	ALavaPlayerStart(const FObjectInitializer& ObjectInitializer);
+
+};
